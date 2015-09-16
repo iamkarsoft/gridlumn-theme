eb62fc1fa7cea1969f2c6e7613e53b65a019facc
Kofi A <karsoft92@gmail.com>
2015-09-16 10:30:48 +0000

    inc folder and files created, and new functions added to function file

--- functions.php
+++ functions.php
@@ -7,7 +7,56 @@
 
 if(! isset($content_width))
 		$content_width=654; /* pixels */;
-		
 
+if(! function_exists('gridlumn_setup')):
+/**
+*setting up theme defaults
+*
+*@since gridlumn 1.0
+*/
+
+function gridlumn_setup(){
+/**
+*custom functions
+*/
+require (get_template_directory().'/inc/tweak.php');
+
+/**
+*Making theme available for translation
+*/
+
+load_theme_textdomain('gridlumn',get_template_directory().'/languages');
+
+/**
+*Enable support for aside post format
+*/
+
+add_theme_support('post-formats',array('aside'));
+
+/**
+*custom template tags
+*/
+
+require (get_template_directory().'inc/template-tags.php');
+
+/**
+*default post and comments RSS
+*/
+
+add_theme_support('automatic-feed-links');
+
+/**
+*navigation style using wordpress navigation menu
+*/
+
+register_nav_menus(array(
+            'primary'=>__('Primary Menu','gridlumn'),
+            ));
+
+
+
+}
+endif;
+add_action('after_setup_theme','gridlumn_setup');
 
 
---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*The header for our theme
+*display all of the <head> section
+*@package gridlumn
+*@since gridlumn 1.0
+*/
+?>
+
+<!doctype html>
+    <!-- [if IE 8] -->
+        <html id="ie8" <?php language_attributes(); ?>>
+    <!-- [endif] -->
+    <!--[if !(ie 8)] > <!-->
+
+    <head>
+        <meta charset="<?php bloginfo('charset')" ; ?>" />
+        <meta name="viewport" content="width=device-width" />
+
+       <?php
+        /**
+        *navigation style using wordpress navigation menu
+        */
+
+
+
+       ?>
+
+
