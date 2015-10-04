173f52c99ba9b8dae7dbd54e77a30d884e4aaa88
iamkarsoft <karsoft92@gmail.com>
2015-10-04 18:52:08 +0000

    uploading other addional files like the customizer js and other content files

---
+++ template-parts/content-page-sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+    /**
+    *template file for page with sidebar
+    */
+    ?>
+
+
+
+
+        <div class="col-md-12" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <?php the_content(); ?>
+        </div>
+
+
+
+
+
+
+
+
+
